9c3be3eae073629af926457dc1f89664fbc02651
akilpath <56412097+akilpath@users.noreply.github.com>
2022-10-27 22:05:11 -0400

    Added skeleton

--- src/util/SwerveModule.c
+++ src/util/SwerveModule.c
@@ -1,4 +1,33 @@
-task main()
-{
+#include "PID.c"
 
+
+
+struct SwerveModule{
+	int motorOneIndex;
+	int motorTwoIndex;
+
+	float targetSpeed;
+	float targetAngle;
+
+	struct PIDController controllerOne;
+	struct PIDController controllerTwo;
 }
+
+void initModule(struct SwerveModule *swerve);
+void setAngle(struct SwerveModule *swerve, float angle);
+void setSpeed(struct SwerveModule *swerve, float speed);
+
+void setPower(struct SwerveModule *swerve,float motorOneP, float motorTwoP)
+{
+	motor[swerve -> motorOneIndex] = motorOneP;
+	motor[swerve -> motorTwoIndex] = motorTwoP;
+}
+
+void initModule(struct SwerveModule *swerve)
+{}
+
+void setAngle(struct SwerveModule *swerve, float angle)
+{}
+
+void setSpeed(struct SwerveModule *swerve, float speed)
+{}
