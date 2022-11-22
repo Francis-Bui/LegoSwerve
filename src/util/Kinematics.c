bd98230baecd0fc922d42d7350d475e2f442e0df
akilpath <56412097+akilpath@users.noreply.github.com>
2022-11-21 21:59:00 -0500

    Finished vector math for swerve

--- src/util/Kinematics.c
+++ src/util/Kinematics.c
@@ -1,7 +1,8 @@
 #include "Constants.c"
 #include "Vec2.c"
 
-
+float* getMotorPowers(float clockwiseP, float forwardP);
+void normalizeMotorVectors(Vec2 *m1, Vec2 *m2);
 float hypot(float x, float y);
 
 float moduleAngleSpeed[2];
@@ -31,10 +32,41 @@ float* getMotorPowers(float clockwiseP, float forwardP)
     Vec2 mOneUVec = Vec2_createVector(1/sqrt(2), 1/sqrt(2));
     Vec2 mTwoUVec = Vec2_createVector(-1/sqrt(2), 1/sqrt(2));
 
+    Vec2 mOneResultant = Vec2_projectOnto(powerVec, mOneUVec);
+    Vec2 mTwoResultant = Vec2_projectOnto(powerVec, mTwoUVec);
+    Vec2_scale(&mOneResultant, sqrt(2));
+    vec2_scale(&mTwoResultant, sqrt(2));
+    normalizeMotorVectors(&mOneResultant, &mTwoResultant);
 
+    float motorPowers[2] = {0};
+    motorPowers[0] = mOneResultant.magnitude;
+    motorPowers[1] = mTwoResultant.magnitude;
+    return motorPowers;
 }
-
 float hypot(float x, float y)
 {
 	return sqrt(pow(x,2) + pow(y,2));
 }
+
+/**
+ * Takes in two motor vectors and makes sure they don't exceed. btw im akil and im awesome
+ * the possible motor power for them individually. If they do, both vectors are scaled
+ * down proportionally.  
+*/
+void normalizeMotorVectors(Vec2 *m1, Vec2 *m2)
+{
+    if(m1 -> magnitude > 100)
+    {
+        float scalar = 100 / (m1 -> magnitude);
+        Vec2_scale(m1, scalar);
+        Vec2_scale(m2, scalar);
+    }
+
+    if(m2 -> magnitude > 100)
+    {
+        float scalar = 100 / (m2 -> magnitude);
+        Vec2_scale(m1, scalar);
+        Vec2_scale(m2, scalar);
+    }
+}
+
